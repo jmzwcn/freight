5c6b3304cffa38ce7b7ee5a3a153ea774f94864b
Daniel Zhang <jmzwcn@gmail.com>
2018-11-11 23:05:56 +0800

    build.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- src/app/driver/driver.page.ts
+++ src/app/driver/driver.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./driver.page.scss'],
 })
 export class DriverPage implements OnInit {
-
-  constructor() { }
+  users: any;
+  constructor() {
+    this.users = [
+      { 'name': '用户1', 'time': '2018-12-12,16:16', 'from': '三里屯', 'to': '天安门', 'fee': 66.66 },
+      { 'name': '用户2', 'time': '2018-12-12,18:18', 'from': '三里屯', 'to': '天安门', 'fee': 88.88 },
+      { 'name': '用户3', 'time': '2018-12-12,19:19', 'from': '三里屯', 'to': '天安门', 'fee': 99.99 }
+    ];
+  }
 
   ngOnInit() {
   }
 
+  showUserDetail(user) {
+    alert('正在开发中.' + user.name);
+  }
 }
